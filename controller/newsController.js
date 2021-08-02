8b468e7a9be99d14d141197e9e7aca4cd36d3921
Farhan Rabbaanii <farhanrbnn@gmail.com>
2021-08-02 12:39:35 +0700

    add next paging and limit

--- controller/newsController.js
+++ controller/newsController.js
@@ -5,20 +5,30 @@ const cheerio = require('cheerio')
 
 const get_news = async (req, res) => {
 	const rssURL = 'https://finance.detik.com/rss'
+	const page = parseInt(req.query.page)
+	const limit = parseInt(req.query.limit)
+	const startIndex = (page - 1) * limit
+	const endIndex = page * limit
 
 	try {
 		const feed = await parser.parseURL(rssURL)
 		const rssFeed = feed.items
 
 		let payload = []
+		let newsLink = []
 
 		for (let i = 0; i < rssFeed.length; i++) {
-			if(i < 5) {
+			newsLink.push(rssFeed[i].link)
+		}
+
+		const linkSlice = newsLink.slice(startIndex, endIndex)
+
+		for (let i = 0; i < linkSlice.length; i++) {
 				try {
-					const load = await superagent.get(rssFeed[i].link)
+					const load = await superagent.get(linkSlice[i])
 					const $ = cheerio.load(load.text)
 
-					const newsTitle = $('.detail__title').map((i, section)=>{
+					const newsTitle = $('.detail__title').map((i, section) => {
 						const title = $(section).text()
 						return title.trim()
 
@@ -30,28 +40,50 @@ const get_news = async (req, res) => {
 
 					}).get()
 
-					const newsBody = $('.detail__body itp_bodycontent_wrapper').map((i, section) => {
-						const body = $(section).find('p')
-						return body.html()
+					const newsBody = $('.detail__body-text itp_bodycontent').map((i, section) => {
+						const body = $(section).text()
+						return body
+
+					}).get()
+
+					const newsAuthor = $('.detail__author').map((i, section) => {
+						const author = $(section).text()
+						return author
+
+					}).get()
+
+					const newsDate = $('.detail__date').map((i, section) => {
+						const date = $(section).text()
+						return date
+
 					}).get()
 
 					const objPayload = {
 						news_title: newsTitle[0],
 						image: newsImage[0],
-						body: newsBody
+						news_author: newsAuthor[0],
+						news_date: newsDate[0]
 					}
 
 					payload.push(objPayload)
 
 				} catch(err) {
-					console.log(err)
+					return res.status(500).send({
+						'error':err
+					})
 				}
-			}
 
 		}
 
+		const paging = {
+			next: 'http://localhost:3000/api/v1/news?page='+(page + 1)+'&limit='+limit+'',
+			limit: limit
+		}
+
 		return res.status(200).send({
+			paging,
 			payload
+
 		})
 		
 	} catch (err) {
